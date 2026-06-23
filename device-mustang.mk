0553fed8eb4051175f21f03595afaed5c0e3cb9e
Michael Bestas <mkbestas@lineageos.org>
2026-06-23 06:35:30 +0300

    muzel: Import generated overlays from CP2A.260605.012
    Change-Id: I670a6f1bfb1b9639c979a3a84a0881b354e01166

--- device-mustang.mk
+++ device-mustang.mk
@@ -29,7 +29,8 @@ PRODUCT_PACKAGES += \
     FrameworkResOverlayVendorMustang \
     PixelDisplayServiceOverlayProductMustang \
     PixelNfcOverlayMustang \
-    PixelUwbOverlayRG5 \
+    PixelUwbOverlayMT5Muzel \
+    PixelUwbOverlayRG5Laguna \
     PixelWifiOverlay2025Mustang \
     SettingsMustangOverlay \
     SystemUIGoogleOverlayVendorMustang
